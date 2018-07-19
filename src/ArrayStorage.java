2b0b90ebeecf8c9d89df8c05898c6e61bd6ded42
Dmitriy <lammer.90@yandex.ru>
2018-07-19 21:27:13 +0300

    Lesson1_2

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,57 +6,46 @@ import java.util.Comparator;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
-        Arrays.fill(storage, null);
-        //Pam 1111
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
-        storage[size()] = r;
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        for (Resume resume : storage) {
-            if (resume == null) return null;
-            else if (resume.uuid.equals(uuid)) return resume;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
         }
         return null;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) break;
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                storage[i] = null;
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
                 break;
             }
         }
-
-        Arrays.sort(storage, new Comparator<Resume>() {
-            @Override
-            public int compare(Resume o1, Resume o2) {
-                if ((o1 == null && o2 == null) || (o1 != null && o2 != null)) return 0;
-                else if (o1 == null) return 1;
-                else if (o2 == null) return -1;
-                return 0;
-            }
-        });
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, size());
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                return i;
-            }
-        }
-        return storage.length;
+        return size;
     }
 }
